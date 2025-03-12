526d1b7b31db98a860f705ba2994b1032d3d083c
Aryan Dixit <aryandixit984@gmail.com>
2025-03-12 21:35:18 +0530

    Update seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = 'mongodb+srv://iamlearning:4myself784378@aryan0.xzmhc.mongodb.net/crowdtrust_ai';
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 async function uploadCampaign(campaign) {
